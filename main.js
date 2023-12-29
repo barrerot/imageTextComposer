aa233974037b26ec78d68d303fdaa847ef4aec70
Carlos Barrero Martínez <barrerot@gmail.com>
2023-12-29 21:49:33 +0100

    actualizacion

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const ImageTextComposer = require('./ImageTextComposer');
+const ImageTextComposer = require("./ImageTextComposer");
+const moment = require('moment'); // Asegúrate de tener instalado el paquete 'moment'
 
 // Configura estas variables con tus propios valores
-const backgroundImagePath = "./assets/background.png"; // Ruta a la imagen de fondo (puede ser PNG o JPEG)
-const outputPath = './assets/output/output.png';   // Ruta para guardar la imagen resultante
-const text = 'En cada desafío reside una oportunidad para crecer y reafirmar la propia fortaleza';                                // El texto que quieres añadir a la imagen
+const backgroundImagePath = "./assets/background.png"; // Ruta a la imagen de fondo
+const text = 'En cada desafío reside una oportunidad para crecer y reafirmar la propia fortaleza'; // El texto
+
+// Generar un timestamp único con formato año, mes, día, hora, minutos y segundos
+const timestamp = moment().format('YYYYMMDD_HHmmss');
+
+// Concatenar el timestamp con el nombre del archivo de salida
+const outputPath = `./assets/output/${timestamp}_output.png`; 
 
 // Crear una instancia de ImageTextComposer
 const composer = new ImageTextComposer(backgroundImagePath, outputPath);
